cfbef35e6b68b9348b43f954c31ef1042f6fa1c7
Tom Archbold <wiggywombathead@hotmail.co.uk>
2018-08-08 21:04:57 +0100

    Foundations for 2D automata. TODO: GoL alive counting

--- simulation/src/automaton.c
+++ simulation/src/automaton.c
@@ -1,23 +1,132 @@
 #include <stdlib.h>
+#include <math.h>
 #include "global.h"
 #include "automaton.h"
 
 struct automaton *init_automaton(int len, simulate_fn func, int d) {
     struct automaton *ca = malloc(sizeof(struct automaton));
-    ca->cells = calloc(len * d, sizeof(int));
+
+    int total_cells = (int) pow(len, d);
+    ca->cells = calloc(total_cells, sizeof(int));
     
     ca->len = len;
     ca->dimension = d;
     ca->sim = func;
 
-    ca->rects = malloc(sizeof(SDL_Rect) * len);
+    ca->rects = malloc(sizeof(SDL_Rect) * total_cells);
 
-    for (int i = 0; i < len; i++) {
-        ca->rects[i].w = WIN_WIDTH / len;
-        ca->rects[i].h = ca->rects[i].w;
-        ca->rects[i].x = i * ca->rects[i].w;
-        ca->rects[i].y = 0;
+    int c_width = WIN_WIDTH / len;
+
+    if (d == 1) {
+
+        for (int i = 0; i < len; i++) {
+            ca->rects[i].w = c_width;
+            ca->rects[i].h = c_width;
+            ca->rects[i].x = i * c_width;
+            ca->rects[i].y = 0;
+        }
+
+    } else {
+
+        int c_height = WIN_HEIGHT / len;
+
+        for (int i = 0 ; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                ca->rects[i * len + j].w = c_width;
+                ca->rects[i * len + j].h = c_height;
+                ca->rects[i * len + j].x = j * c_width;
+                ca->rects[i * len + j].y = i * c_height;
+            }
+        }
+
     }
 
     return ca;
+}
+
+void rule_90(struct automaton *ca) {
+
+    int total_cells = (int) pow(ca->len, ca->dimension);
+   
+    /* create new array to compute states without changing current ones */
+    int *new = malloc(sizeof(int) * total_cells);
+
+    for (int i = 1; i < ca->len-1; i++) {
+        new[i] = (ca->cells[i-1] + ca->cells[i+1]) % 2;
+    }
+
+    new[0] = ca->cells[1];
+    new[ca->len-1] = ca->cells[ca->len-2];
+
+    ca->cells = new;
+
+}
+
+void gol(struct automaton *ca) {
+
+    int total_cells = (int) pow(ca->len, ca->dimension);
+
+    int *new = calloc(total_cells, sizeof(int));
+    memcpy(new, ca->cells, sizeof(int) * total_cells);
+
+    printf("Current:\n");
+    for (int i = 0; i < ca->len; i++) {
+        for (int  j = 0; j < ca->len; j++) {
+            printf("%d ", ca->cells[i*ca->len+j]);
+        }
+        printf("\n");
+    }
+    printf("----------\nAlive:\n");
+
+    for (int i = 0; i < ca->len; i++) {
+        for (int  j = 0; j < ca->len; j++) {
+
+            int curr = i * ca->len + j;
+            int alive = 0;
+
+            if (i > 0 && j > 0)
+                alive += ca->cells[curr - ca->len - 1];
+
+            if (i > 0)
+                alive += ca->cells[curr - ca->len];
+
+            if (i > 0 && j < ca->len-1)
+                alive += ca->cells[curr - ca->len + 1];
+
+            if (j > 0)
+                alive += ca->cells[curr - 1];
+
+            if (j < ca->len-1)
+                alive += ca->cells[curr + 1];
+
+            if (i < ca->len-1 && j < 0)
+                alive += ca->cells[curr + ca->len - 1];
+
+            if (i < ca->len-1)
+                alive += ca->cells[curr + ca->len];
+
+            if (i < ca->len-1 && j < ca->len-1)
+                alive += ca->cells[curr + ca->len + 1];
+
+            if (ca->cells[curr] == ALIVE) {
+
+                /* death by isolation / overpopulation */
+                if (alive < 2 || alive > 3)
+                    new[curr] = DEAD;
+
+            } else {
+
+                /* reproduction */
+                if (alive == 3)
+                    new[curr] = ALIVE;
+
+            }
+            printf("%d ", alive);
+
+        }
+        printf("\n");
+    }
+
+    ca->cells = new;
+
 }
--- simulation/src/main.c
+++ simulation/src/main.c
@@ -1,67 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
-#include <SDL2/SDL.h>
 #include <time.h>
+
+#include <SDL2/SDL.h>
 
 #include "global.h"
 #include "automaton.h"
 
 int max_gens = 1024;
 
-void rule_01(struct automaton *ca) {
-   
-    /* create new array to compute states without changing current ones */
-    int *new = malloc(sizeof(int) * ca->len);
+extern void rule_90(struct automaton *);
+extern void gol(struct automaton *);
 
-    for (int i = 1; i < ca->len-1; i++) {
-        new[i] = (ca->cells[i-1] + ca->cells[i+1]) % 2;
-    }
-
-    new[0] = ca->cells[1];
-    new[ca->len-1] = ca->cells[ca->len-2];
-
-    ca->cells = new;
-
-}
-/*
-void gol(struct automaton *ca) {
-
-    int *new = malloc(sizeof(int) * ca->len);
-    for (int i = 0; i < ca->len; i++) {
-        for (int j = 0; j < ca->len; j++) {
-
-            int alive = 0;
-
-            alive += ca->cells[i-1][j-1];
-            alive += ca->cells[i-1][j];
-            alive += ca->cells[i-1][j+1];
-            alive += ca->cells[i][j-1];
-            alive += ca->cells[i][j+1];
-            alive += ca->cells[i+1][j-1];
-            alive += ca->cells[i+1][j];
-            alive += ca->cells[i+1][j+1];
-
-        }
-    }
-
-}
-*/
-
-void render(SDL_Renderer *r, struct automaton *ca) {
-
-    /* draw to screen */
-    for (int i = 0; i < ca->len; i++) {
-        if (ca->cells[i] == ALIVE) {
-            SDL_SetRenderDrawColor(r, 255, 255, 255, SDL_ALPHA_OPAQUE);
-        } else {
-            SDL_SetRenderDrawColor(r, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        }
-
-        SDL_RenderFillRect(r, &ca->rects[i]);
-        ca->rects[i].y += ca->rects[i].h;
-    }
-}
+void render(SDL_Renderer *r, struct automaton *);
 
 int main() {
 
@@ -101,24 +54,35 @@ int main() {
     }
 
     SDL_Event e;
-    int is_running = 1;
+    int is_running = true;
     int current_gen = 0;
 
     srand(time(NULL));
+
     int config[1024];
     for (int i = 0; i < 1024; i++) {
-        // config[i] = rand() % 2;
-        if (rand() % 10 == 0)
-            config[i] = ALIVE;
+        config[i] = rand() % 2;
     }
 
-    printf("%ld\n", sizeof(config)/sizeof(config[0]));
     struct automaton *rule1 = init_automaton(
             sizeof(config) / sizeof(int),
-            &rule_01,
+            &rule_90,
             1
     );
     rule1->cells = config;
+
+    int gol_conf[] = {
+        0,0,0,0,
+        0,1,0,0,
+        0,0,0,0,
+        0,0,0,0
+    };
+    struct automaton *conway = init_automaton(
+            sqrt(sizeof(gol_conf) / sizeof(int)),
+            &gol,
+            2
+    );
+    conway->cells = gol_conf;
 
     while (is_running) {
 
@@ -140,20 +104,11 @@ int main() {
             break;
         }
 
-        if (rule1->rects[0].y + rule1->rects[0].h > win_height)
-            continue;
+        /* render and update automaton */
+        render(renderer, conway);
 
-        /* render */
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-
-        render(renderer, rule1);
-
-        SDL_RenderPresent(renderer);
-        /* update objects */
-        rule1->sim(rule1);
+        SDL_Delay(100);
         current_gen++;
-        
-        SDL_Delay(6);
     }
 
     SDL_DestroyRenderer(renderer);
@@ -162,4 +117,44 @@ int main() {
     SDL_Quit();
 
     return 0;
+}
+
+void render(SDL_Renderer *r, struct automaton *ca) {
+
+    if (ca->dimension == 1) {
+
+        for (int i = 0; i < ca->len; i++) {
+            if (ca->cells[i] == ALIVE) {
+                SDL_SetRenderDrawColor(r, 255, 255, 255, SDL_ALPHA_OPAQUE);
+            } else {
+                SDL_SetRenderDrawColor(r, 0, 0, 0, SDL_ALPHA_OPAQUE);
+            }
+
+            SDL_RenderFillRect(r, &ca->rects[i]);
+            ca->rects[i].y += ca->rects[i].h;
+        }
+
+    } else {
+
+        SDL_SetRenderDrawColor(r, 0, 0, 0, SDL_ALPHA_OPAQUE);
+
+        for (int i = 0; i < ca->len; i++) {
+            for (int j= 0; j < ca->len; j++) {
+
+                int offset = i * ca->len + j;
+
+                if (ca->cells[offset] == ALIVE) {
+                    SDL_SetRenderDrawColor(r, 255, 255, 255, SDL_ALPHA_OPAQUE);
+                } else {
+                    SDL_SetRenderDrawColor(r, 0, 0, 0, SDL_ALPHA_OPAQUE);
+                }
+
+                SDL_RenderFillRect(r, &ca->rects[offset]);
+            }
+        }
+
+    }
+
+    SDL_RenderPresent(r);
+    ca->sim(ca);
 }
